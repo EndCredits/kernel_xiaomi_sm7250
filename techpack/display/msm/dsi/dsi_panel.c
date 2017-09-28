d7e5321d59e37e8f0fafe9e9879444f44fbf373d
Adrian Salido <salidoa@google.com>
2017-09-28 12:40:59 -0700

    drm/msm: allow init delay and lp11 init options
    Enable device tree options to enable lp11 init before panel on and a
    delay to be added before any activity on lp11.
    
    Bug: 139655049
    Change-Id: Ieeca80a17211cba6b0de62f78128750c3b5b4093
    Signed-off-by: Adrian Salido <salidoa@google.com>
    Signed-off-by: Ken Huang <kenbshuang@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -2226,6 +2226,7 @@ error:
 static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 {
 	struct dsi_parser_utils *utils = &panel->utils;
+	u32 val;
 
 	panel->ulps_feature_enabled =
 		utils->read_bool(utils->data, "qcom,ulps-enabled");
@@ -2250,6 +2251,15 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 
 	panel->reset_gpio_always_on = utils->read_bool(utils->data,
 			"qcom,platform-reset-gpio-always-on");
+
+	if (!utils->read_u32(utils->data,
+				  "qcom,mdss-dsi-init-delay-us",
+				  &val)) {
+		pr_debug("Panel init delay specified: %d\n", val);
+		panel->init_delay_us = val;
+	} else {
+		panel->init_delay_us = 0;
+	}
 
 	return 0;
 }
@@ -4347,6 +4357,9 @@ int dsi_panel_prepare(struct dsi_panel *panel)
 			goto error;
 		}
 	}
+
+	if (panel->init_delay_us)
+		usleep_range(panel->init_delay_us, panel->init_delay_us);
 
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_PRE_ON);
 	if (rc) {
