cadbb7a651981a308bb7d7b113a14bf4d45832a5
Gao Xiang <gaoxiang25@huawei.com>
2018-10-30 15:07:28 -0700

    lib/lz4: update LZ4 decompressor module
    Update the LZ4 compression module based on LZ4 v1.8.3 in order for the
    erofs file system to use the newest LZ4_decompress_safe_partial() which
    can now decode exactly the nb of bytes requested [1] to take place of the
    open hacked code in the erofs file system itself.
    
    Currently, apart from the erofs file system, no other users use
    LZ4_decompress_safe_partial, so no worry about the interface.
    
    In addition, LZ4 v1.8.x boosts up decompression speed compared to the
    current code which is based on LZ4 v1.7.3, mainly due to shortcut
    optimization for the specific common LZ4-sequences [2].
    
    lzbench testdata (tested in kirin710, 8 cores, 4 big cores
    at 2189Mhz, 2GB DDR RAM at 1622Mhz, with enwik8 testdata [3]):
    
    Compressor name         Compress. Decompress. Compr. size  Ratio Filename
    memcpy                   5004 MB/s  4924 MB/s   100000000 100.00 enwik8
    lz4hc 1.7.3 -9             12 MB/s   653 MB/s    42203253  42.20 enwik8
    lz4hc 1.8.0 -9             12 MB/s   908 MB/s    42203096  42.20 enwik8
    lz4hc 1.8.3 -9             11 MB/s   965 MB/s    42203094  42.20 enwik8
    
    [1] https://github.com/lz4/lz4/issues/566
        https://github.com/lz4/lz4/commit/08d347b5b217b011ff7487130b79480d8cfdaeb8
    
    [2] v1.8.1 perf: slightly faster compression and decompression speed
        https://github.com/lz4/lz4/commit/a31b7058cb97e4393da55e78a77a1c6f0c9ae038
        v1.8.2 perf: slightly faster HC compression and decompression speed
        https://github.com/lz4/lz4/commit/45f8603aae389d34c689d3ff7427b314071ccd2c
        https://github.com/lz4/lz4/commit/1a191b3f8d26b50a7c1d41590b529ec308d768cd
    
    [3] http://mattmahoney.net/dc/textdata.html
        http://mattmahoney.net/dc/enwik8.zip
    
    Link: http://lkml.kernel.org/r/1537181207-21932-1-git-send-email-gaoxiang25@huawei.com
    Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>
    Tested-by: Guo Xuenan <guoxuenan@huawei.com>
    Cc: Colin Ian King <colin.king@canonical.com>
    Cc: Yann Collet <yann.collet.73@gmail.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Fang Wei <fangwei1@huawei.com>
    Cc: Chao Yu <yuchao0@huawei.com>
    Cc: Miao Xie <miaoxie@huawei.com>
    Cc: Sven Schmidt <4sschmid@informatik.uni-hamburg.de>
    Cc: Kyungsik Lee <kyungsik.lee@lge.com>
    Cc: <weidu.du@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -141,9 +141,6 @@ static FORCE_INLINE int LZ4_decompress_generic(
 		 * space in the output for those 18 bytes earlier, upon
 		 * entering the shortcut (in other words, there is a
 		 * combined check for both stages).
-		 *
-		 * The & in the likely() below is intentionally not && so that
-		 * some compilers can produce better parallelized runtime code
 		 */
 		if ((endOnInput ? length != RUN_MASK : length <= 8)
 		   /*
@@ -153,7 +150,7 @@ static FORCE_INLINE int LZ4_decompress_generic(
 		   && likely((endOnInput ? ip < shortiend : 1) &
 			     (op <= shortoend))) {
 			/* Copy the literals */
-			LZ4_memcpy(op, ip, endOnInput ? 16 : 8);
+			memcpy(op, ip, endOnInput ? 16 : 8);
 			op += length; ip += length;
 
 			/*
@@ -172,9 +169,9 @@ static FORCE_INLINE int LZ4_decompress_generic(
 			    (offset >= 8) &&
 			    (dict == withPrefix64k || match >= lowPrefix)) {
 				/* Copy the match. */
-				LZ4_memcpy(op + 0, match + 0, 8);
-				LZ4_memcpy(op + 8, match + 8, 8);
-				LZ4_memcpy(op + 16, match + 16, 2);
+				memcpy(op + 0, match + 0, 8);
+				memcpy(op + 8, match + 8, 8);
+				memcpy(op + 16, match + 16, 2);
 				op += length + MINMATCH;
 				/* Both stages worked, load the next token. */
 				continue;
@@ -390,7 +387,7 @@ _copy_match:
 				while (op < copyEnd)
 					*op++ = *match++;
 			} else {
-				LZ4_memcpy(op, match, mlen);
+				memcpy(op, match, mlen);
 			}
 			op = copyEnd;
 			if (op == oend)
@@ -404,7 +401,7 @@ _copy_match:
 			op[2] = match[2];
 			op[3] = match[3];
 			match += inc32table[offset];
-			LZ4_memcpy(op + 4, match, 4);
+			memcpy(op + 4, match, 4);
 			match -= dec64table[offset];
 		} else {
 			LZ4_copy8(op, match);
