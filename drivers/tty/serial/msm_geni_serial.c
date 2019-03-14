154a1572e74678a8da83f47850afef27de70ef22
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-14 14:30:46 +0900

    msm_geni_serial: skip flushing tx upon shutdown
    This is causing runtime PM to malfunction and prevents suspend indefinitely.
    
    This might be HAL related, but since flushing data by manually powering on
    the serial is unnecessary upon shutdown, skip it instead.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -2317,15 +2317,6 @@ static void msm_geni_serial_shutdown(struct uart_port *uport)
 
 	IPC_LOG_MSG(msm_port->ipc_log_misc, "%s:\n", __func__);
 	/* Stop the console before stopping the current tx */
-	if (uart_console(uport)) {
-		console_stop(uport->cons);
-		disable_irq(uport->irq);
-	} else {
-		msm_geni_serial_power_on(uport);
-		wait_for_transfers_inflight(uport);
-		msm_geni_serial_stop_tx(uport);
-	}
-
 	if (!uart_console(uport)) {
 		if (msm_port->ioctl_count) {
 			int i;
