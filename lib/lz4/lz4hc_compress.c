ad214531465fe59afb3f1c27fb1c94624a2305dc
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-14 04:46:28 +0000

    lz4: Mark unused functions as __maybe_unused
    This fixes following warnings:
    ../lib/lz4/lz4hc_compress.c:619:13: warning: unused function 'LZ4_resetStreamHC' [-Wunused-function]
    static void LZ4_resetStreamHC(LZ4_streamHC_t *LZ4_streamHCPtr, int compressionLevel)
                ^
    ../lib/lz4/lz4hc_compress.c:715:12: warning: unused function 'LZ4_compress_HC_continue' [-Wunused-function]
    static int LZ4_compress_HC_continue(
               ^
    ../lib/lz4/lz4hc_compress.c:732:12: warning: unused function 'LZ4_saveDictHC' [-Wunused-function]
    static int LZ4_saveDictHC(
               ^
    3 warnings generated.
    
    Change-Id: I1ddc0ee1810af6bb4b48a4530a33f5cfc910bc0a
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- lib/lz4/lz4hc_compress.c
+++ lib/lz4/lz4hc_compress.c
@@ -616,7 +616,7 @@ EXPORT_SYMBOL(LZ4_compress_HC);
 /**************************************
  *	Streaming Functions
  **************************************/
-void LZ4_resetStreamHC(LZ4_streamHC_t *LZ4_streamHCPtr, int compressionLevel)
+__maybe_unused void LZ4_resetStreamHC(LZ4_streamHC_t *LZ4_streamHCPtr, int compressionLevel)
 {
 	LZ4_streamHCPtr->internal_donotuse.base = NULL;
 	LZ4_streamHCPtr->internal_donotuse.compressionLevel = (unsigned int)compressionLevel;
@@ -713,7 +713,7 @@ static int LZ4_compressHC_continue_generic(
 		inputSize, maxOutputSize, ctxPtr->compressionLevel, limit);
 }
 
-int LZ4_compress_HC_continue(
+__maybe_unused int LZ4_compress_HC_continue(
 	LZ4_streamHC_t *LZ4_streamHCPtr,
 	const char *source,
 	char *dest,
@@ -731,7 +731,7 @@ EXPORT_SYMBOL(LZ4_compress_HC_continue);
 
 /* dictionary saving */
 
-int LZ4_saveDictHC(
+__maybe_unused int LZ4_saveDictHC(
 	LZ4_streamHC_t *LZ4_streamHCPtr,
 	char *safeBuffer,
 	int dictSize)
