59f31effd4ab7c167a9f4d793ebf28e0a8fe3376
Yue Hu <huyue2@coolpad.com>
2022-09-23 10:11:22 +0800

    UPSTREAM: erofs: support on-disk compressed fragments data
    Introduce on-disk compressed fragments data feature.
    
    This approach adds a new field called `h_fragmentoff' in the per-file
    compression header to indicate the fragment offset of each tail pcluster
    or the whole file in the special packed inode.
    
    Similar to ztailpacking, it will also find and record the 'headlcn'
    of the tail pcluster when initializing per-inode zmap for making
    follow-on requests more easy.
    
    Signed-off-by: Yue Hu <huyue2@coolpad.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Link: https://lore.kernel.org/r/YzHKxcFTlHGgXeH9@B-P7TQMD6M-0146.local
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- fs/erofs/sysfs.c
+++ fs/erofs/sysfs.c
@@ -74,6 +74,7 @@ EROFS_ATTR_FEATURE(device_table);
 EROFS_ATTR_FEATURE(compr_head2);
 EROFS_ATTR_FEATURE(sb_chksum);
 EROFS_ATTR_FEATURE(ztailpacking);
+EROFS_ATTR_FEATURE(fragments);
 
 static struct attribute *erofs_feat_attrs[] = {
 	ATTR_LIST(zero_padding),
@@ -84,6 +85,7 @@ static struct attribute *erofs_feat_attrs[] = {
 	ATTR_LIST(compr_head2),
 	ATTR_LIST(sb_chksum),
 	ATTR_LIST(ztailpacking),
+	ATTR_LIST(fragments),
 	NULL,
 };
 
