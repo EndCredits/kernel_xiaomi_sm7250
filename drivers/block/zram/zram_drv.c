85084b689dee6c308b0e622781de2dacd8932be4
EndCredits <alicization.han@gmail.com>
2022-09-29 08:36:10 +0800

    Revert "block: genhd: add 'groups' argument to device_add_disk"
    This reverts commit 7d15e0f20f3aee65597ac6ccce00c0b02d5ea8ec.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2010,7 +2010,8 @@ static int zram_add(void)
 		blk_queue_max_write_zeroes_sectors(zram->disk->queue, UINT_MAX);
 
 	zram->disk->queue->backing_dev_info->capabilities |= BDI_CAP_STABLE_WRITES;
-	device_add_disk(NULL, zram->disk);
+	disk_to_dev(zram->disk)->groups = zram_disk_attr_groups;
+	add_disk(zram->disk);
 
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
 
