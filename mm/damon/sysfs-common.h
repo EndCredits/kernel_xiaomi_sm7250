829fa555f6c81bfc5f8f562a626f64b9539fa574
SeongJae Park <sj@kernel.org>
2022-10-26 22:59:39 +0000

    mm/damon/sysfs: move unsigned long range directory to common module
    The implementation of unsigned long type range directories can be reused
    by multiple DAMON sysfs directories including those for DAMON-based
    Operation Schemes and the range of number of monitoring regions.  Move the
    code into the files for DAMON sysfs common logics.
    
    Link: https://lkml.kernel.org/r/20221026225943.100429-9-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/damon/sysfs-common.h
+++ mm/damon/sysfs-common.h
@@ -17,3 +17,16 @@
 }
 
 extern struct mutex damon_sysfs_lock;
+
+struct damon_sysfs_ul_range {
+	struct kobject kobj;
+	unsigned long min;
+	unsigned long max;
+};
+
+struct damon_sysfs_ul_range *damon_sysfs_ul_range_alloc(
+		unsigned long min,
+		unsigned long max);
+void damon_sysfs_ul_range_release(struct kobject *kobj);
+
+extern struct kobj_type damon_sysfs_ul_range_ktype;
