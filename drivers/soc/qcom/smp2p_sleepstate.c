b4f829aaddcc723fe31a5520483b9afa38c4a3ac
EndCredits <alicization.han@gmail.com>
2023-01-21 09:38:58 +0800

    Revert " soc: qcom: smp2p_sleepstate: Add suspend delay"
    This reverts commit 4b8fbdfefe74add89189182d895be165a79399c5.

--- drivers/soc/qcom/smp2p_sleepstate.c
+++ drivers/soc/qcom/smp2p_sleepstate.c
@@ -32,7 +32,6 @@ static int sleepstate_pm_notifier(struct notifier_block *nb,
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
 		qcom_smem_state_update_bits(state, AWAKE_BIT, 0);
-		usleep_range(10000, 10500); /* Tuned based on SMP2P latencies */
 		break;
 
 	case PM_POST_SUSPEND:
