5e656a0bec8bb709a2dd6d4547b79e7573037d14
EndCredits <endcredits@crepuscular-aosp.icu>
2024-01-20 15:11:00 +0800

    Revert "BACKPORT: zram: break the strict dependency from lzo"
    This reverts commit 06705c5ac518bb4717ce10fb042d572c6d590422.

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -19,10 +19,8 @@
 #include "zcomp.h"
 
 static const char * const backends[] = {
-#if IS_ENABLED(CONFIG_CRYPTO_LZO)
 	"lzo",
 	"lzo-rle",
-#endif
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 	"lz4",
 #endif
